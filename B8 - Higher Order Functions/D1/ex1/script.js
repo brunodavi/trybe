dd765686876d7259af551431eff3ee1d5fb7073a
Bruno Davi <brunodaviandrade2000@gmail.com>
2021-04-30 15:50:10 -0300

    Parte 1 - 8.1

---
+++ B8 - Higher Order Functions/D1/ex1/script.js
@@ -0,0 +1,60 @@
+// EX HOF 1
+
+const repeat = (number, action) => {
+  for (let count = 0; count <= number; count += 1) {
+    action(count);
+  }
+};
+
+repeat(5, console.log);
+
+
+// EX HOF 2
+
+const repeat = (number, action) => {
+  for (let count = 0; count <= number; count += 1) {
+    action(count);
+  }
+};
+
+repeat(3, (number) => {
+  if (number % 2 === 0) {
+    console.log(number, 'is even');
+  }
+});
+
+
+// EX HOF 3
+
+const repeat = (number, action) => {
+  for (let count = 0; count <= number; count += 1) {
+    action(count);
+  }
+};
+
+const isEven = (number) => {
+  if (number % 2 === 0) {
+    console.log(number, 'is even');
+  }
+};
+
+const isOdd = (number) => {
+  if ((number % 2) > 0) {
+    console.log(number, 'is odd');
+  }
+};
+
+repeat(3, isEven); // Testa quais números serão pares;
+repeat(3, isOdd); // Testa quais números serão ímpares;
+
+
+// EX HOF 4
+
+const a = () => console.log('Acordando!!');
+const b = () => console.log('Bora tomar café!!');
+const c = () => console.log('Partiu dormir!!');
+const d = (f) => f()
+
+d(a);
+d(b);
+d(c);
