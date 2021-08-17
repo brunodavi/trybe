dfa687671e9642b6dd18a10f48c13abe4ab48afa
Bruno Davi <brunodaviandrade2000@gmail.com>
2021-08-17 18:07:05 -0300

    Branch B12

---
+++ B12/D2/ex1/my-form-2.0/src/component/Info.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import countries from './countries'
+
+export default class Info extends Component {
+  render() {
+    const { handleClick, states } = this.props
+    return (
+      <fieldset>
+        <legend>Informações Pessoais</legend>
+
+        <label><p>Nome:</p>
+          <input name="name" type="text" value={states.name}
+            onChange={handleClick} maxLength="40" required />
+        </label>
+
+        <label><p>Email:</p> 
+          <input name="email" type="email" value={states.email}
+          maxLength="50" onChange={handleClick} required
+          pattern="[\w._]{6,30}@[a-z]{5,10}\.com(.br)?" />
+        </label>
+
+        <label><p>CPF:</p> 
+          <input name="cpf" type="text" value={states.cpf}
+          maxLength="11" onChange={handleClick} required />
+        </label>
+
+        <label><p>Endereço:</p>
+          <input name="address" type="text" value={states.address}
+          maxLength="200" onChange={handleClick} required />
+        </label>
+
+        <label><p>Cidade:</p>
+          <input name="city" type="text" value={states.city}
+          maxLength="28" onBlur={handleClick} onChange={handleClick} required />
+        </label>
+        
+        <select name="countries" value={states.countries} onChange={handleClick} required >
+          {countries.map((country, i) => <option key={ i } >{country.nome}</option>)}
+        </select>
+
+        <div className="radios">
+          <label>
+            <input type="radio" name="type" value="apartamento" onChange={handleClick} /> Apartamento
+          </label>
+
+          <label>
+            <input type="radio" name="type" value="casa" onChange={handleClick} required /> Casa
+          </label>
+        </div>
+
+      </fieldset>
+    )
+  }
+}
