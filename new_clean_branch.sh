2214feef4b7022073495c4b6fdf2966b65cd6fb1
Bruno Davi <brunodaviandrade2000@gmail.com>
2021-08-17 18:02:18 -0300

    Criar branch com pasta através de script

---
+++ new_clean_branch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+then
+  echo 'Não existem argumentos'
+  exit
+fi
+
+BRANCHS="$(grep -Pom 1 "$1" <<< $(git branch -a))"
+
+if [ "$1" != "$BRANCHS" ]
+then
+  echo "Criando branch com a pasta $1/..."
+  git checkout -b "$1" B0
+  git checkout master -- "$1/"
+  git commit -m "Branch $1"
+  git push -u origin "$1"
+  git checkout master
+else
+  echo 'Essa branch já existe'
+fi
+
