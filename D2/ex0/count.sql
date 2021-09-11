a8cfd8edd8ebd3c6460334d498f865b465d50913
Bruno Davi <brunodaviandrade2000@gmail.com>
2021-09-11 11:20:23 -0300

    Reorganizando Conteúdo

---
+++ D2/ex0/count.sql
@@ -0,0 +1,17 @@
+-- Quantas senhas temos cadastradas nessa tabela?
+SELECT 
+    COUNT(password)
+FROM
+    sakila.staff;
+
+-- Quantas pessoas temos no total trabalhando para nossa empresa?
+SELECT 
+    COUNT(active)
+FROM
+    sakila.staff;
+
+-- Quantos emails temos cadastrados nessa tabela?
+SELECT 
+    COUNT(email)
+FROM
+    sakila.staff;
